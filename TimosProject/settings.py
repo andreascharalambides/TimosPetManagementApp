78211ba7ece7998acb9858a5d33a359b217bd324
Carln02 <carlnjeim@gmail.com>
2024-12-12 22:13:20 +0100

    Update settings.py

--- TimosProject/settings.py
+++ TimosProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-_l@9$9)_ztr($q$u2e8ixy#_i!vz982i(+t%l1_eg18le6swv=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['164.90.166.75']
+ALLOWED_HOSTS = ['timospetapp.fr', 'www.timospetapp.fr', '127.0.0.1', 'localhost']
 
 
 # Application definition
