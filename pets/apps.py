cd0d242b2805081ffcf4fe85aac83b01f75a3591
Carln02 <carlnjeim@gmail.com>
2024-12-13 13:24:51 +0100

    Changes

--- pets/apps.py
+++ pets/apps.py
@@ -1,9 +1,13 @@
+import threading
 from django.apps import AppConfig
-
 
 class PetsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "pets"
 
     def ready(self):
-        import pets.signals
+        from .tasks import start_scheduler
+
+        thread = threading.Thread(target=start_scheduler)
+        thread.daemon = True  # Ensures the thread will exit when the main program exits
+        thread.start()
--- pets/tasks.py
+++ pets/tasks.py
@@ -1,19 +1,20 @@
-from celery import shared_task
+import schedule
+import time
+import json
+from datetime import timedelta
 from django.utils import timezone
 from django.conf import settings
-from pywebpush import webpush, WebPushException
-from datetime import timedelta
 from .models import Task
-import json
+from pywebpush import webpush, WebPushException
 
-
-@shared_task
 def send_task_notifications():
     now = timezone.now()
-    reminder_time = now + timedelta(minutes=15)
+    reminder_time = now + timedelta(minutes=10)
 
     # Filter tasks starting in the next 15 minutes
     tasks_soon = Task.objects.filter(start_date__range=(now, reminder_time))
+
+    print(tasks_soon)
 
     for task in tasks_soon:
         user = task.pet.user
@@ -40,3 +41,12 @@ def send_task_notifications():
                 )
             except WebPushException as ex:
                 print("Failed to send notification:", ex)
+
+# Schedule the task
+schedule.every(1).minutes.do(send_task_notifications)
+
+def start_scheduler():
+    print("Starting the scheduler...")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
